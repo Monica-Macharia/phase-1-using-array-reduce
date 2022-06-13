6dce9e7f2ec9a6a6b149b6e44f653a24a9794de8
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-06-13 16:15:06 +0300

    commit tree a665d79f061d55db132acf1f39aaf08d009b9496

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// function
+//function summation(batteryBatches) {
+// let sum = 0;
+//for (let i = 0; i < batteryBatches.length; i++){
+// sum = sum + batteryBatches[i];}
+//return sum;}
+//console.log(summation(batteryBatches))
+
+//using reduce function
+const totalBatteries = batteryBatches.reduce(function(sum, currentItem) {
+    sum = sum + currentItem;
+    return sum;
+}, 0);
+console.log(totalBatteries);
